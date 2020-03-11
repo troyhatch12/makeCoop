2138e8811ce1a66f8a27a875c4b8445181258a1f
troyhatch12 <troyhatchard@gmail.com>
2020-03-11 14:39:18 -0600

    From data passes to query and redirects

--- index.php
+++ index.php
@@ -10,27 +10,40 @@ if ($conn->connect_error){
 	die("connection failed: " . $conn->connect_error);
 }
 
+echo("<html>");
 
-//Add person form
-echo( "
-	<html>
-		<form action='index.php' method='post'>
-			<h3>Name: </h3><input type='text' name='name'>
-			<h3>Phone: </h3><input type='text' name='phone'>
-			<h3>CreditNum: </h3><input type='text' name='cc'>
-			<h3>Address: </h3><input type='text'name='address'>
-			<input type='submit'>
-		</form>
-	</html>
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$address = $_POST['address'];
+	$phone = $_POST['phone'];
+	$creditNum = $_POST['cc'];
+
+	//insert data into customer
+	$addQry = "INSERT INTO customer (Name, Address, Phone, CreditNum)
+				VALUES ('$name', '$address', '$phone', '$creditNum');";
+	if (mysqli_query($conn, $addQry)){
+		echo("
+			<h1>New Customer created!</h1>
+			<button href = 'index.php'>Go Back</button>
 		");
-//insert data into customer
-//need to fill with vals from html
-$addQry = "INSERT INTO customer (Name, Address, Phone, CreditNum)
-			VALUES ('troy', '69 ayy lmao', '1010101010', '5253546235768798' )";
-if (mysqli_query($conn, $addQry)){
-	echo("New Customer created!");
+	} else {
+		echo("There was an error adding customer: " . mysqli_error($conn));
+	}
 } else {
-	echo("There was an error adding customer: " . mysqli_error($conn));
+	//Add person form
+	echo( "
+			<form action='index.php' method='post'>
+				<h3>Name: </h3><input type='text' name='name'>
+				<h3>Phone: </h3><input type='text' name='phone'>
+				<h3>CreditNum: </h3><input type='text' name='cc'>
+				<h3>Address: </h3><input type='text'name='address'>
+				<input type='submit' name='submit'>
+			</form>
+		</html>
+			");
+
 }
+
+echo "</html>";
 mysqli_close($conn);
 ?>
