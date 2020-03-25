85aff492f533d817ae87743a7eb82796d3d8026e
NeilSchoe <neilschoeman2@gmail.com>
2020-03-24 18:14:01 -0600

    receipt, item, purchase tables added

--- makeCoop.sql
+++ makeCoop.sql
@@ -1,16 +1,41 @@
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `customer` (
   `MemberId` smallint(3) NOT NULL AUTO_INCREMENT,
-  `Name` char(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `Address` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `Phone` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `CreditNum` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Name` char(25) DEFAULT NULL,
+  `Address` varchar(100) DEFAULT NULL,
+  `Phone` varchar(10) DEFAULT NULL,
+  `CreditNum` int(16) DEFAULT NULL,
   PRIMARY KEY (`MemberId`)
-);
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer`
---
-
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'Glarb','0001 The Swamp','1010101010','5253546235768798'),(3,'Troy','4','7801231231','1231231231231231'),(5,'Neil','asdf','7891234123','1273819283172839'),(10,'Billy Bob','Booboo Avenue','7382937492','9393939393939393'),(11,'Gill Fish','Coral Link','7839274928','3737373737373737'),(13,'Troy','ahahahahah','7802141214','hahahah'),(14,'Gary','myhouse','1234566789','1234123412341234'),(15,'gary2','90jk',';lkj','lkjkl'),(16,'gary2','90jk',';lkj','lkjkl'),(17,'yoo','House','123','12345');
+INSERT INTO `customer` VALUES (6,'test2','road way blvd','48585747',324234243);
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `ItemId` int(4) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `Dept` varchar(15) DEFAULT NULL,
+  `Price` decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY (`ItemId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchase` (
+  `ReceiptId` int(6) NOT NULL,
+  `ItemId` int(4) NOT NULL,
+  PRIMARY KEY (`ReceiptId`,`ItemId`),
+  KEY `ItemId` (`ItemId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `receipt` (
+  `ReceiptId` int(6) NOT NULL AUTO_INCREMENT,
+  `Total` decimal(6,2) DEFAULT NULL,
+  `MemberId` smallint(3) NOT NULL,
+  PRIMARY KEY (`ReceiptId`),
+  KEY `MemberId` (`MemberId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
