a62e7e9696b2f76dcb362a4c2374e4abae079e3d
NeilSchoe <neilschoeman2@gmail.com>
2020-04-01 14:52:35 -0600

    added pInsert.php

---
+++ pInsert.php
@@ -0,0 +1,39 @@
+<?php
+require_once('printhtml.php');
+$config = require_once('config.php');
+
+//Create Connection and check
+$conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+if ($conn->connect_error){
+	die("connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['addItem'])) {
+	$itemId = $_POST['itemId'];
+	$receiptId = $_POST['receiptId'];
+	//make sure that nothing is blank before creating the query
+	if ($ItemId && $receiptId){
+		//insert data into customer
+		$addQry = "INSERT INTO purchase (ReceiptId, ItemId)
+					VALUES ('$receiptId' , '$itemId);";
+		if (mysqli_query($conn, $addQry)){
+			echo("
+				<p>New Purchase created!<p>
+				<button onclick='redirect()'>Go Back</button>
+				<script>
+					const redirect = () => {
+						window.location = 'shop.php';
+					}
+				</script>
+			");
+			$_POST['submit'] = '';
+		} else {
+			echo("There was an error adding customer: " . mysqli_error($conn));
+		}
+	} else {
+		echo "Error: Values cannot be blank";
+	}
+};
+
+mysqli_close($conn);
+?>
