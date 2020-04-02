0515f9b2fb3579b2f45224115055415f95b4a9be
NeilSchoe <neilschoeman2@gmail.com>
2020-04-02 13:57:18 -0600

    Dummy data
    added dummy.sql filled with dummy data

---
+++ dummy.sql
@@ -0,0 +1,122 @@
+-- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: makeCoop
+-- ------------------------------------------------------
+-- Server version	8.0.18
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,ANSI' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table "customer"
+--
+
+DROP TABLE IF EXISTS "customer";
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE "customer" (
+  "MemberId" smallint(3) NOT NULL AUTO_INCREMENT,
+  "Name" char(25) DEFAULT NULL,
+  "Address" varchar(100) DEFAULT NULL,
+  "Phone" varchar(10) DEFAULT NULL,
+  "CreditNum" int(16) DEFAULT NULL,
+  PRIMARY KEY ("MemberId")
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table "customer"
+--
+
+LOCK TABLES "customer" WRITE;
+/*!40000 ALTER TABLE "customer" DISABLE KEYS */;
+INSERT INTO "customer" VALUES (8,'troy','109301','3993391',3024902),(6,'test2','road way blvd','48585747',324234243),(7,'neil','393939o st','39399393',493839),(9,'Barry','orange juice blvd','0103099491',2147483647),(10,'Shoja','emperor palp way','9119119111',2147483647);
+/*!40000 ALTER TABLE "customer" ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table "item"
+--
+
+DROP TABLE IF EXISTS "item";
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE "item" (
+  "ItemId" int(4) NOT NULL AUTO_INCREMENT,
+  "Name" varchar(100) NOT NULL,
+  "Dept" varchar(15) DEFAULT NULL,
+  "Price" decimal(6,2) DEFAULT NULL,
+  PRIMARY KEY ("ItemId")
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table "item"
+--
+
+LOCK TABLES "item" WRITE;
+/*!40000 ALTER TABLE "item" DISABLE KEYS */;
+INSERT INTO "item" VALUES (1,'celery','produce',69.00),(2,'can o beans','grocery',420.00),(3,'steak','meat',393.02),(4,'ham','meat',4.32),(5,'bagels','bakery',59.32),(6,'bread','bakery',24.32),(7,'oatmeal','grocery',202.42),(8,'onion','produce',3.10),(9,'apple','produce',3030.10),(10,'ground beef','meat ',3.40),(11,'salsa','grocery',30.43),(12,'windex','housewares',34.11),(13,'toilet brush','housewares',95.32);
+/*!40000 ALTER TABLE "item" ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table "purchase"
+--
+
+DROP TABLE IF EXISTS "purchase";
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE "purchase" (
+  "ReceiptId" int(6) NOT NULL,
+  "ItemId" int(4) NOT NULL,
+  "Quantity" smallint(6) NOT NULL,
+  PRIMARY KEY ("ReceiptId","ItemId"),
+  KEY "ItemId" ("ItemId")
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table "purchase"
+--
+
+LOCK TABLES "purchase" WRITE;
+/*!40000 ALTER TABLE "purchase" DISABLE KEYS */;
+/*!40000 ALTER TABLE "purchase" ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table "receipt"
+--
+
+DROP TABLE IF EXISTS "receipt";
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE "receipt" (
+  "ReceiptId" int(6) NOT NULL AUTO_INCREMENT,
+  "MemberId" smallint(3) NOT NULL,
+  "Date" datetime NOT NULL,
+  PRIMARY KEY ("ReceiptId"),
+  KEY "MemberId" ("MemberId")
+);
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table "receipt"
+--
+
+LOCK TABLES "receipt" WRITE;
+/*!40000 ALTER TABLE "receipt" DISABLE KEYS */;
+/*!40000 ALTER TABLE "receipt" ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-02 13:56:02
